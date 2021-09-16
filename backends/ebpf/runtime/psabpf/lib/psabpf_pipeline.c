69879b32ae39e9d4a1b6ece39cd8729236d0f38e
elfadel <bonfohelfadel@gmail.com>
2021-09-16 15:09:36 +0200

    XDP: Adjust the ifindex value to the maximum size of the DEVMAP (#214)
    * Adjust the ifindex value to the maximum size of the DEVMAP
    
    * Grow the DEVMAP size and apply modulo to bpf_redirect_map()

--- backends/ebpf/runtime/psabpf/lib/psabpf_pipeline.c
+++ backends/ebpf/runtime/psabpf/lib/psabpf_pipeline.c
@@ -75,7 +75,13 @@ static int xdp_port_add(__u32 pipeline_id, char *intf)
     }
     devmap_val.ifindex = ifindex;
     devmap_val.bpf_prog.fd = eg_prog_fd;
-    ret = bpf_map_update_elem(devmap_fd, &ifindex, &devmap_val, 0);
+    if (ifindex > DEVMAP_SIZE) {
+        fprintf(stderr,
+            "Warning: the index(=%d) of the interface %s is higher than the DEVMAP size (=%d)\n"
+            "Applying modulo ... \n", ifindex, intf, DEVMAP_SIZE);
+    }
+    int index = ifindex % DEVMAP_SIZE;
+    ret = bpf_map_update_elem(devmap_fd, &index, &devmap_val, 0);
     if (ret) {
         return ret;
     }
