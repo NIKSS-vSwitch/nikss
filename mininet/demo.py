bce15788bc14be98300f6d6151efa2a679923093
Tomasz Osinski <tomasz.osinski@intel.com>
2022-04-27 14:51:40 +0200

    Polish scripts and add documentation

--- mininet/demo.py
+++ mininet/demo.py
@@ -2,7 +2,6 @@
 
 import socket
 
-
 from lib.psabpf_mn import P4Host, PSAeBPFSwitch
 
 from mininet.net import Mininet
@@ -20,24 +19,20 @@ class MyCustomTopo(Topo):
         Topo.__init__(self, **opts)
 
         switch = self.addSwitch('s1',
-                                bpf_path="out.o",
+                                bpf_path="simple_switch.o",
                                 enable_tracing = True)
         for h in range(2):
             host = self.addHost('h%d' % (h + 1),
-                                #ip = "10.0.%d.10/24" % h,
                                 mac = '00:04:00:00:00:%02x' % (h + 1))
             self.addLink(host, switch, (h + 1), (h + 2))
 
 
 def main():
     topo = MyCustomTopo()
-
     net = Mininet(topo = topo,
                   host = P4Host,
                   switch = PSAeBPFSwitch,
                   controller = None)
-
-
     net.start()
 
     sleep(1)
@@ -48,14 +43,14 @@ def main():
     h2 = net.get('h2')
     h2.setARP("10.0.0.1", "00:04:00:00:00:01")
 
-    print(socket.if_nameindex())
-
     s1 = net.get('s1')
+    # Install table entries to forward traffic between hosts
     s1.cmd("psabpf-ctl table add pipe 0 ingress_tbl_fwd id 1 key {} data {}".format(h2.MAC(), socket.if_nametoindex("s1-eth3")))
     s1.cmd("psabpf-ctl table add pipe 0 ingress_tbl_fwd id 1 key {} data {}".format(h1.MAC(), socket.if_nametoindex("s1-eth2")))
 
     CLI( net )
     net.stop()
+
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
--- mininet/lib/psabpf_mn.py
+++ mininet/lib/psabpf_mn.py
@@ -3,6 +3,10 @@ from mininet.net import Mininet
 from mininet.node import Switch, Host
 from mininet.log import setLogLevel, info, error, debug
 
+"""
+This module is an extension of `mininet.node`. It provides Mininet objects for P4Host and PSA-eBPF switch. 
+"""
+
 class P4Host(Host):
     """
     P4Host is imported from p4_mininet.py (BMv2). 
@@ -35,6 +39,7 @@ class P4Host(Host):
         ))
         print("**********")
 
+
 class PSAeBPFSwitch(Switch):
     """
     PSA-eBPF switch. Requires psabpf-ctl.
@@ -43,32 +48,34 @@ class PSAeBPFSwitch(Switch):
     # Device ID is also used as a PSA-eBPF pipeline ID. 
     device_id = 0
 
-    def __init__(self, name, bpf_path = None, enable_tracing = False, **kwargs):
+    def __init__(self, name, bpf_path = None, device_id=None, **kwargs):
         Switch.__init__(self, name, **kwargs)
         assert(bpf_path)
 
         self.bpf_path = bpf_path
-
+        if device_id is not None:
+            self.device_id = device_id
+            PSAeBPFSwitch.device_id = max(PSAeBPFSwitch.device_id, device_id)
+        else:
+            self.device_id = PSAeBPFSwitch.device_id
+            PSAeBPFSwitch.device_id += 1
         
     def start(self, controllers):
         info("Starting PSA-eBPF switch {}.\n".format(self.name))
-        self.cmd("psabpf-ctl pipeline load id {} {}".format(self.device_id, self.bpf_path), verbose=True)
+        self.cmd("psabpf-ctl pipeline load id {} {}".format(self.device_id, self.bpf_path))
 
         for port, intf in self.intfs.items():
             if not "s1-" in str(intf):
                 continue
             info("Attaching port {} to PSA-eBPF switch {}.\n".format(intf, self.name))
-            self.cmd("psabpf-ctl add-port pipe {} dev {}".format(self.device_id, intf), verbose=True)
+            self.cmd("psabpf-ctl add-port pipe {} dev {}".format(self.device_id, intf))
 
 
     def stop(self, deleteIntfs=True):
-        self.cmd("psabpf-ctl pipeline unload id {}".format(self.device_id), verbose=True)
+        for port, intf in self.intfs.items():
+            if not "s1-" in str(intf):
+                continue
+            info("Detaching port {} from PSA-eBPF switch {}.\n".format(intf, self.name))
+            self.cmd("psabpf-ctl del-port pipe {} dev {}".format(self.device_id, intf))
+        self.cmd("psabpf-ctl pipeline unload id {}".format(self.device_id))
         super( PSAeBPFSwitch, self ).stop( deleteIntfs )
-
-    def attach(self, intf):
-        info("Attaching port {} to PSA-eBPF switch {}.\n".format(intf, self.name))
-
-    def detach(self, intf):
-        info("Detaching port {} from PSA-eBPF switch {}.\n".format(intf, self.name))
-
-
