e452541cb353e522a268f18b8198ab980cb387ce
Mateusz Kossakowski <mateusz.kossakowski@orange.com>
2022-05-16 11:59:37 +0200

    Add register read command (#27)

---
+++ CLI/register.c
@@ -0,0 +1,270 @@
+/*
+ * Copyright 2022 Orange
+ * Copyright 2022 Warsaw University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <jansson.h>
+
+#include "register.h"
+#include <psabpf.h>
+
+static int parse_dst_register(int *argc, char ***argv, const char **register_name,
+                             psabpf_context_t *psabpf_ctx, psabpf_register_context_t *ctx)
+{
+    if (*argc < 1) {
+        fprintf(stderr, "too few parameters\n");
+        return EINVAL;
+    }
+
+    if (is_keyword(**argv, "id")) {
+        NEXT_ARGP_RET();
+        fprintf(stderr, "id: register access not supported\n");
+        return ENOTSUP;
+    } else if (is_keyword(**argv, "name")) {
+        NEXT_ARGP_RET();
+        fprintf(stderr, "name: register access not supported yet\n");
+        return ENOTSUP;
+    } else {
+        if (register_name != NULL)
+            *register_name = **argv;
+        int error_code = psabpf_register_ctx_name(psabpf_ctx, ctx, **argv);
+        if (error_code != NO_ERROR)
+            return error_code;
+    }
+
+    NEXT_ARGP();
+    return NO_ERROR;
+}
+
+static int parse_register_index(int *argc, char ***argv, psabpf_register_entry_t *entry)
+{
+    if (!is_keyword(**argv, "index"))
+        return NO_ERROR; /* index is optional */
+    NEXT_ARGP_RET();
+
+    bool has_any_index = false;
+    while (*argc > 0) {
+        if (has_any_index) {
+            if (is_keyword(**argv, "value"))
+                return NO_ERROR;
+        }
+
+        int err = translate_data_to_bytes(**argv, entry, CTX_REGISTER_INDEX);
+        if (err != NO_ERROR)
+            return err;
+
+        has_any_index = true;
+        NEXT_ARGP();
+    }
+
+    return NO_ERROR;
+}
+
+static int build_struct_json(json_t *parent, psabpf_register_context_t *ctx, psabpf_register_entry_t *entry,
+                             psabpf_struct_field_t * (*get_next_field)(psabpf_register_context_t*, psabpf_register_entry_t*))
+{
+    psabpf_struct_field_t *field;
+    while ((field = get_next_field(ctx, entry)) != NULL) {
+        /* To build flat structure of output JSON just remove this and next conditional
+         * statement. In other words, preserve only condition and instructions below it:
+         *      if (psabpf_digest_get_field_type(field) != DIGEST_FIELD_TYPE_DATA) continue; */
+        if (psabpf_struct_get_field_type(field) == PSABPF_STRUCT_FIELD_TYPE_STRUCT_START) {
+            json_t *sub_struct = json_object();
+            if (sub_struct == NULL) {
+                fprintf(stderr, "failed to prepare message sub-object JSON\n");
+                return ENOMEM;
+            }
+            if (json_object_set(parent, psabpf_struct_get_field_name(field), sub_struct)) {
+                fprintf(stderr, "failed to add message sub-object JSON\n");
+                json_decref(sub_struct);
+                return EPERM;
+            }
+
+            int ret = build_struct_json(sub_struct, ctx, entry, get_next_field);
+            json_decref(sub_struct);
+            if (ret != NO_ERROR)
+                return ret;
+
+            continue;
+        }
+
+        if (psabpf_struct_get_field_type(field) == PSABPF_STRUCT_FIELD_TYPE_STRUCT_END)
+            return NO_ERROR;
+
+        if (psabpf_struct_get_field_type(field) != PSABPF_STRUCT_FIELD_TYPE_DATA)
+            continue;
+
+        const char *encoded_data = convert_bin_data_to_hexstr(psabpf_struct_get_field_data(field),
+                                                              psabpf_struct_get_field_data_len(field));
+        if (encoded_data == NULL) {
+            fprintf(stderr, "not enough memory\n");
+            return ENOMEM;
+        }
+        const char *field_name = psabpf_struct_get_field_name(field);
+        if (field_name == NULL)
+            field_name = "";
+        json_object_set_new(parent, field_name, json_string(encoded_data));
+        free((void *) encoded_data);
+    }
+
+    return NO_ERROR;
+}
+
+static int build_entry(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry,
+                       json_t *json_entry)
+{
+    json_t *index = json_object();
+    json_t *value = json_object();
+    if (json_entry == NULL || index == NULL || value == NULL) {
+        fprintf(stderr, "failed to prepare register in JSON\n");
+        return ENOMEM;
+    }
+
+    json_object_set_new(json_entry, "index", index);
+    json_object_set_new(json_entry, "value", value);
+
+    int ret = build_struct_json(value, ctx, entry, psabpf_register_get_next_value_field);
+    if (ret != NO_ERROR) {
+        fprintf(stderr, "failed to build register value in JSON\n");
+        return EINVAL;
+    }
+
+    ret = build_struct_json(index, ctx, entry, psabpf_register_get_next_index_field);
+    if (ret != NO_ERROR) {
+        fprintf(stderr, "failed to build register index in JSON\n");
+        return EINVAL;
+    }
+
+    return NO_ERROR;
+}
+
+static int get_and_print_register_json(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry,
+                                       const char *register_name, bool entry_has_index)
+{
+    int ret = EINVAL;
+    json_t *root = json_object();
+    json_t *instance_name = json_object();
+    json_t *entries = json_array();
+    if (root == NULL || instance_name == NULL || entries == NULL) {
+        fprintf(stderr, "failed to prepare JSON\n");
+        goto clean_up;
+    }
+
+    json_object_set(root, register_name, entries);
+
+    if (entry_has_index) {
+        if (psabpf_register_get(ctx, entry) != NO_ERROR) {
+            goto clean_up;
+        }
+        json_t *json_entry = json_object();
+        ret = build_entry(ctx, entry, json_entry);
+        json_array_append_new(entries, json_entry);
+    } else {
+        psabpf_register_entry_t *iter;
+        while ((iter = psabpf_register_get_next(ctx)) != NULL) {
+            json_t *json_entry = json_object();
+            ret = build_entry(ctx, iter, json_entry);
+            json_array_append_new(entries, json_entry);
+            psabpf_register_entry_free(iter);
+            if (ret != NO_ERROR)
+                break;
+        }
+    }
+
+    if (ret != NO_ERROR) {
+        fprintf(stderr, "failed to build register JSON: %s\n", strerror(ret));
+        goto clean_up;
+    }
+
+    json_dumpf(root, stdout, JSON_INDENT(4) | JSON_ENSURE_ASCII);
+    ret = NO_ERROR;
+
+clean_up:
+    json_decref(entries);
+    json_decref(instance_name);
+    json_decref(root);
+
+    return ret;
+}
+
+int do_register_get(int argc, char **argv)
+{
+    int ret = EINVAL;
+    const char *register_name = NULL;
+    psabpf_context_t psabpf_ctx;
+    psabpf_register_context_t ctx;
+    psabpf_register_entry_t entry;
+
+    psabpf_context_init(&psabpf_ctx);
+    psabpf_register_ctx_init(&ctx);
+    psabpf_register_entry_init(&entry);
+
+    if (parse_pipeline_id(&argc, &argv, &psabpf_ctx) != NO_ERROR)
+        goto clean_up;
+
+    if (parse_dst_register(&argc, &argv, &register_name, &psabpf_ctx, &ctx) != NO_ERROR)
+        goto clean_up;
+
+    bool register_index_provided = (argc >= 1 && is_keyword(*argv, "index"));
+    if (register_index_provided) {
+        if (parse_register_index(&argc, &argv, &entry) != NO_ERROR)
+            goto clean_up;
+    }
+
+    if (argc > 0) {
+        fprintf(stderr, "%s: unused argument\n", *argv);
+        goto clean_up;
+    }
+
+    ret = get_and_print_register_json(&ctx, &entry, register_name, register_index_provided);
+
+clean_up:
+    psabpf_register_entry_free(&entry);
+    psabpf_register_ctx_free(&ctx);
+    psabpf_context_free(&psabpf_ctx);
+
+    return ret;
+}
+
+int do_register_set(int argc, char **argv) {
+    // TODO implement
+    return NO_ERROR;
+}
+
+int do_register_reset(int argc, char **argv) {
+    // TODO implement
+    return NO_ERROR;
+}
+
+int do_register_help(int argc, char **argv)
+{
+    (void) argc; (void) argv;
+    fprintf(stderr,
+            "Usage: %1$s register get pipe ID REGISTER [index DATA]\n"
+            "Unimplemented commands:\n"
+            "       %1$s register set pipe ID REGISTER index DATA value REGISTER_VALUE\n"
+            "       %1$s register reset pipe ID REGISTER index DATA\n"
+            "\n"
+            "       REGISTER := { id REGISTER_ID | name REGISTER | REGISTER_FILE }\n"
+            "       REGISTER_VALUE := { DATA }\n"
+            "",
+            program_name);
+
+    return NO_ERROR;
+}
---
+++ lib/psabpf_register.c
@@ -0,0 +1,264 @@
+/*
+ * Copyright 2022 Orange
+ * Copyright 2022 Warsaw University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <bpf/bpf.h>
+
+#include <psabpf.h>
+#include "common.h"
+#include "btf.h"
+#include "bpf_defs.h"
+
+void psabpf_register_ctx_init(psabpf_register_context_t *ctx) {
+    if (ctx == NULL)
+        return;
+
+    memset(ctx, 0, sizeof(psabpf_register_context_t));
+}
+
+void psabpf_register_ctx_free(psabpf_register_context_t *ctx) {
+    if (ctx == NULL)
+        return;
+
+    free_btf(&ctx->btf_metadata);
+    close_object_fd(&(ctx->reg.fd));
+    free_struct_field_descriptor_set(&ctx->key_fds);
+    free_struct_field_descriptor_set(&ctx->value_fds);
+}
+
+static int parse_key_type(psabpf_register_context_t *ctx)
+{
+    uint32_t type_id = 0;
+    if (ctx->btf_metadata.btf != NULL) {
+        type_id = psabtf_get_member_type_id_by_name(ctx->btf_metadata.btf, ctx->reg.btf_type_id, "key");
+    }
+
+    return parse_struct_type(&ctx->btf_metadata, type_id, ctx->reg.key_size, &ctx->key_fds);
+}
+
+static int parse_value_type(psabpf_register_context_t *ctx)
+{
+    uint32_t type_id = 0;
+    if (ctx->btf_metadata.btf != NULL) {
+        type_id = psabtf_get_member_type_id_by_name(ctx->btf_metadata.btf, ctx->reg.btf_type_id, "value");
+    }
+
+    return parse_struct_type(&ctx->btf_metadata, type_id, ctx->reg.value_size, &ctx->value_fds);
+}
+
+int psabpf_register_ctx_name(psabpf_context_t *psabpf_ctx, psabpf_register_context_t *ctx, const char *name) {
+    if (psabpf_ctx == NULL || ctx == NULL || name == NULL)
+        return EINVAL;
+
+    if (load_btf(psabpf_ctx, &ctx->btf_metadata) != NO_ERROR) {
+        fprintf(stderr, "couldn't find a BTF info\n");
+    }
+
+    int ret = open_bpf_map(psabpf_ctx, name, &ctx->btf_metadata, &ctx->reg);
+    if (ret != NO_ERROR) {
+        fprintf(stderr, "couldn't open a register %s\n", name);
+        return ret;
+    }
+
+    if (parse_key_type(ctx) != NO_ERROR) {
+        fprintf(stderr, "%s: couldn't get key BTF info of a Register instance\n", name);
+        return EOPNOTSUPP;
+    }
+
+    if (parse_value_type(ctx) != NO_ERROR) {
+        fprintf(stderr, "%s: couldn't get value BTF info of a Register instance\n", name);
+        return EOPNOTSUPP;
+    }
+
+    return NO_ERROR;
+}
+
+void psabpf_register_entry_init(psabpf_register_entry_t *entry) {
+    if (entry == NULL)
+        return;
+
+    memset(entry, 0, sizeof(psabpf_register_entry_t));
+}
+
+void psabpf_register_entry_free(psabpf_register_entry_t *entry) {
+    if (entry == NULL)
+        return;
+
+    free_struct_field_set(&entry->entry_key);
+
+    if (entry->raw_key != NULL)
+        free(entry->raw_key);
+    entry->raw_key = NULL;
+
+    if (entry->raw_value != NULL)
+        free(entry->raw_value);
+    entry->raw_value = NULL;
+}
+
+int psabpf_register_entry_set_key(psabpf_register_entry_t *entry, const void *data, size_t data_len) {
+    if (entry == NULL)
+        return EINVAL;
+    if (data == NULL || data_len < 1)
+        return ENODATA;
+
+    int ret = struct_field_set_append(&entry->entry_key, data, data_len);
+    if (ret != NO_ERROR)
+        fprintf(stderr, "couldn't append key to an entry: %s\n", strerror(ret));
+    return ret;
+}
+
+static void *allocate_key_buffer(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry)
+{
+    if (entry->raw_key != NULL)
+        return entry->raw_key;  /* already allocated */
+
+    entry->raw_key = malloc(ctx->reg.key_size);
+    if (entry->raw_key == NULL)
+        fprintf(stderr, "not enough memory\n");
+
+    return entry->raw_key;
+}
+
+static void *allocate_value_buffer(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry)
+{
+    if (entry->raw_value != NULL)
+        return entry->raw_value;
+
+    entry->raw_value = malloc(ctx->reg.value_size);
+    if (entry->raw_value == NULL)
+        fprintf(stderr, "not enough memory\n");
+
+    return entry->raw_value;
+}
+
+psabpf_struct_field_t * psabpf_register_get_next_value_field(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry)
+{
+    if (ctx == NULL || entry == NULL)
+        return NULL;
+
+    psabpf_struct_field_descriptor_t *fd;
+    fd = get_struct_field_descriptor(&ctx->value_fds, entry->current_field_id);
+    if (fd == NULL) {
+        entry->current_field_id = 0;
+        return NULL;
+    }
+
+    entry->current_field.type = fd->type;
+    entry->current_field.data_len = fd->data_len;
+    entry->current_field.name = fd->name;
+    entry->current_field.data = entry->raw_value + fd->data_offset;
+
+    entry->current_field_id = entry->current_field_id + 1;
+
+    return &entry->current_field;
+}
+
+psabpf_struct_field_t * psabpf_register_get_next_index_field(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry)
+{
+    if (ctx == NULL || entry == NULL)
+        return NULL;
+
+    psabpf_struct_field_descriptor_t *fd;
+    fd = get_struct_field_descriptor(&ctx->key_fds, entry->current_field_id);
+    if (fd == NULL) {
+        entry->current_field_id = 0;
+        return NULL;
+    }
+
+    entry->current_field.type = fd->type;
+    entry->current_field.data_len = fd->data_len;
+    entry->current_field.name = fd->name;
+    entry->current_field.data = entry->raw_key + fd->data_offset;
+
+    entry->current_field_id = entry->current_field_id + 1;
+
+    return &entry->current_field;
+}
+
+psabpf_register_entry_t * psabpf_register_get_next(psabpf_register_context_t *ctx)
+{
+    if (ctx == NULL)
+        return NULL;
+
+    if (allocate_key_buffer(ctx, &ctx->current_entry) == NULL)
+        return NULL;
+
+    /* on first call ctx->prev_entry_ke must be NULL */
+    if (bpf_map_get_next_key(ctx->reg.fd, ctx->prev_entry_key, ctx->current_entry.raw_key) != 0) {
+        /* no more entries, prepare for next iteration */
+        if (ctx->prev_entry_key != NULL)
+            free(ctx->prev_entry_key);
+        ctx->prev_entry_key = NULL;
+
+        return NULL;
+    }
+
+    if (ctx->prev_entry_key == NULL) {
+        ctx->prev_entry_key = malloc(ctx->reg.key_size);
+        if (ctx->prev_entry_key == NULL) {
+            fprintf(stderr, "not enough memory\n");
+            return NULL;
+        }
+    }
+
+    memcpy(ctx->prev_entry_key, ctx->current_entry.raw_key, ctx->reg.key_size);
+
+    if (allocate_value_buffer(ctx, &ctx->current_entry) == NULL)
+        return NULL;
+
+    int ret = bpf_map_lookup_elem(ctx->reg.fd, ctx->current_entry.raw_key, ctx->current_entry.raw_value);
+    if (ret != NO_ERROR) {
+        fprintf(stderr, "failed to read Register entry: %s\n", strerror(ret));
+        return NULL;
+    }
+
+    return &ctx->current_entry;
+}
+
+int psabpf_register_get(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry)
+{
+    if (allocate_key_buffer(ctx, entry) == NULL)
+        return ENOMEM;
+
+    int ret = construct_struct_from_fields(&entry->entry_key, &ctx->key_fds, entry->raw_key, ctx->reg.key_size);
+    if (ret != NO_ERROR)
+        return ret;
+
+    if (allocate_value_buffer(ctx, entry) == NULL)
+        return ENOMEM;
+
+    ret = bpf_map_lookup_elem(ctx->reg.fd, entry->raw_key, entry->raw_value);
+    if (ret != 0) {
+        ret = errno;
+        fprintf(stderr, "failed to read Register entry: %s\n", strerror(ret));
+        return ret;
+    }
+
+    return NO_ERROR;
+}
+
+int psabpf_register_set(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry) {
+    // TODO implement
+    return NO_ERROR;
+}
+
+int psabpf_register_reset(psabpf_register_context_t *ctx, psabpf_register_entry_t *entry) {
+    // TODO implement
+    return NO_ERROR;
+}
