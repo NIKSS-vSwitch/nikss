3e92aed8b1aaa9a7a51bd0c5241e0b4ae534f858
Tomasz Osiński <osinstom@gmail.com>
2022-12-05 08:36:45 +0100

    Add Dockerfile for NIKSS (#81)
    * Add Dockerfile for NIKSS
    
    * Add missing packages required for demo
    
    * Build on master only
    
    Co-authored-by: Jan Palimąka <25018671+tatry@users.noreply.github.com>

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM p4lang/third-party:latest as builder
+
+ARG CMAKE_BUILD_TYPE=RELEASE
+# No questions asked during package installation.
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update
+RUN apt-get install -y --no-install-recommends build-essential ca-certificates python3 flex bison \
+     make cmake gcc git libgmp-dev libelf-dev zlib1g-dev libjansson-dev libboost-graph-dev libboost-iostreams1.71-dev \
+     libgc-dev libfl-dev pkg-config libcap-dev
+
+COPY . /nikss
+WORKDIR /nikss
+RUN chmod u+x /nikss/scripts/docker-build.sh && /nikss/scripts/docker-build.sh
+
+FROM ubuntu:20.04 as runtime
+LABEL authors="Tomasz Osiński <osinstom@gmail.com>, Jan Palimąka <jan.palimaka95@gmail.com>"
+
+RUN apt-get update
+RUN apt-get install -y --no-install-recommends ca-certificates python3 libelf-dev iproute2 clang llvm gcc libboost-iostreams1.71-dev make libjansson4 curl iperf iputils-ping
+
+COPY --from=builder /usr/local/bin/p4c-ebpf /usr/local/bin/p4c-ebpf
+COPY --from=builder /usr/local/bin/p4c /usr/local/bin/p4c
+COPY --from=builder /usr/local/bin/nikss-ctl /usr/local/bin/nikss-ctl
+COPY --from=builder /usr/local/sbin/bpftool /usr/local/sbin/bpftool
+COPY --from=builder /usr/local/include/ebpf_kernel.h /usr/local/include/ebpf_kernel.h
+COPY --from=builder /usr/local/include/ebpf_common.h /usr/local/include/ebpf_common.h
+COPY --from=builder /usr/local/include/psa.h /usr/local/include/psa.h
+COPY --from=builder /usr/include/bpf /usr/include/bpf
+COPY --from=builder /usr/local/share/p4c /usr/local/share/p4c
+
+
+WORKDIR /nikss
---
+++ scripts/docker-build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e  # Exit on error.
+set -x  # Make command execution verbose
+
+function build_nikss() {
+  git submodule update --init --recursive
+  ./build_libbpf.sh
+  if [ -e build ]; then /bin/rm -rf build; fi
+  mkdir -p build
+  cd build
+  cmake "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}" ..
+  make -j$(nproc)
+  make install
+}
+
+function build_bpftool() {
+  git clone --recurse-submodules https://github.com/libbpf/bpftool.git /tmp/bpftool
+  cd /tmp/bpftool/src
+  make -j$(nproc)
+  make install
+}
+
+function build_p4c() {
+  git clone --recursive https://github.com/p4lang/p4c.git /tmp/p4c
+  cd /tmp/p4c
+  git checkout "$1"
+  if [ -e build ]; then /bin/rm -rf build; fi
+  mkdir -p build
+  cd build
+  cmake  .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_EBPF=ON -DENABLE_UBPF=OFF -DENABLE_DPDK=OFF \
+            -DENABLE_P4C_GRAPHS=OFF -DENABLE_P4TEST=OFF -DENABLE_GTESTS=OFF
+  make -j$(nproc)
+  make install
+
+  # install libbpf headers globally
+  cd /tmp/p4c/backends/ebpf/runtime/contrib/libbpf/src
+  make install_headers
+
+  # install headers required by p4c-ebpf globally
+  # we won't need to add includes paths to clang
+  cp /tmp/p4c/backends/ebpf/runtime/ebpf_common.h /usr/local/include
+  cp /tmp/p4c/backends/ebpf/runtime/ebpf_kernel.h /usr/local/include
+  cp /tmp/p4c/backends/ebpf/runtime/psa.h /usr/local/include
+}
+
+build_nikss
+build_bpftool
+build_p4c 35b9dbf6d95183d41236d7ebbb4d63dcc401c37d
+
+# cleanup
+rm -rf /tmp/bpftool /tmp/p4c
+rm -rf /p4c /var/cache/apt/* /var/lib/apt/lists/*
